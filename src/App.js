38c88ebc90fd11699b58519abd59ee1a8c67361a
Ramon-Carrillo <carrillojrramon@gmail.com>
2023-01-09 11:24:29 -0600

    added skip link for keyboard users

--- src/App.js
+++ src/App.js
@@ -14,6 +14,9 @@ import './styles/ice-cream.scss'
 function App() {
   return (
     <Router>
+      <a href="#main" className="skip-link">
+        Skip to main content
+      </a>
       <HelmetProvider>
         <Header />
         <Routes>
